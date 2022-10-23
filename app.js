e0b54ef77ef8c36e3d9ac7e14a3a19b2028693e1
Kakabay <2kakabayashyrberdyew@gmail.com>
2022-10-23 16:47:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+function slidesPlugin(num) {
+    const slides = document.querySelectorAll(".slide")
+
+    for (const slide of slides) {
+        slide.addEventListener("click", () => {
+            clearActiveClasses()
+            
+            slide.classList.add("active")
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove("active")
+        })
+    }
+
+    slides[num].classList.add('active')
+}
+
+slidesPlugin()
